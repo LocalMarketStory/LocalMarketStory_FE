8c1f75e833bb9a0b703e850397f0d6eea7dc87d5
east_filmm <ehdvlf001@naver.com>
2023-11-15 13:17:45 +0900

    feat: 프롬프트 수정 및 출력 형식 변경

--- src/pages/GptPage.jsx
+++ src/pages/GptPage.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { OpenAIApi, Configuration } from "openai";
 import useStore from "../stores/store";
 
 const GptPage = () => {
   const list = useStore((state) => state.list);
-  const [gptResult, setGptResult] = useState(""); // GPT 결과를 저장할 state
+  const [gptResult, setGptResult] = useState("");
   const [loading, setLoading] = useState(false);
+  const [plan1, setPlan1] = useState("");
+  const [plan2, setPlan2] = useState("");
 
   useEffect(() => {
-    // useStore의 list에 변화가 있을 때마다 GPT 호출
     if (list.length > 0) {
-      // GPT 호출 함수
       const fetchGptResult = async () => {
         const KEY = import.meta.env.VITE_OPENAI_API_KEY;
         const configuration = new Configuration({
@@ -18,37 +18,64 @@ const GptPage = () => {
         });
         const openai = new OpenAIApi(configuration);
 
-        const prompt1 =
+        const prompt =
           `${list.join(", ")}` +
-          "를 활용하여 대구여행 경로를 추천받고 싶다. 다음 조건에 맞게 추천해줘 1. 반드시 list의 여행지를 모두 포함할것 2.출력형식은 다음을 반드시 따를것.3. 여행지는 5개를 넘지 않을것. 4. 각 여행지의 최적의 루트를 제공할것. 5. 세부 시간계획까지 모두 작성해줄것 6. 1박 2일/ 2박 3일 두가지 버전으로 작성해 줄것" +
+          "를 활용하여 대구여행 경로를 추천받고 싶다. 다음 조건에 맞게 추천해줘 1. 반드시 list의 여행지를 모두 포함할 것 2. 출력형식은 다음을 반드시 따를 것. 3. 여행지는 3개를 넘지 않을 것. 4. 각 여행지의 최적의 루트를 제공할 것. 5. 세부 시간 계획까지 모두 작성해줄 것 6. 1박 2일 / 2박 3일 두 가지 버전으로 작성해 줄 것. 7. 여행지 중에 서문시장 또는 칠성야시장은 반드시 포함시킬 것. 8. 같은 여행지는 일정 중에 두 번 방문하지 않을 것." +
           "출력형식 [여행계획(1박/2일)] 여행지: 여행지1 -> 여행지2 -> 여행지3 -> ...,일정 : 여행지1(00:00~00:00), 여행지2 ... ";
 
         try {
-          setLoading(true); // 로딩 시작
+          setLoading(true);
           const result = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: prompt1,
+            prompt: prompt,
             temperature: 0.5,
             max_tokens: 1000,
           });
 
           setGptResult(result.data.choices[0].text);
         } catch (error) {
-          console.error("Error:", error);
+          console.error("에러:", error);
         } finally {
-          setLoading(false); // 로딩 종료
+          setLoading(false);
         }
       };
-      // GPT 호출 함수 실행
+
       fetchGptResult();
     }
   }, [list]);
 
+  useEffect(() => {
+    const parseResults = () => {
+      const marker = "[여행계획(2박/3일)]";
+      const markerIndex = gptResult.indexOf(marker);
+
+      if (markerIndex !== -1) {
+        setPlan1(gptResult.slice(0, markerIndex + marker.length));
+        setPlan2(gptResult.slice(markerIndex + marker.length));
+      }
+    };
+
+    parseResults();
+  }, [gptResult]);
+
   return (
     <div>
-      <h2>GPT Result</h2>
-      {loading ? <p>Generating...</p> : <p>{gptResult}</p>}
-      <button onClick={() => setLoading(true)}>Generate</button>
+      <h2>Trip Plane</h2>
+      {loading ? (
+        <p>생성 중...</p>
+      ) : (
+        <div>
+          <div>
+            <h3>계획 1</h3>
+            <p>{plan1}</p>
+          </div>
+          <div>
+            <h3>계획 2</h3>
+            <p>{plan2}</p>
+          </div>
+        </div>
+      )}
+      <button onClick={() => setLoading(true)}>생성</button>
     </div>
   );
 };
